5e79f89d4c99075673b67a108ba302a011b97b95
LTnano <124283968+LTnano@users.noreply.github.com>
2025-07-18 12:51:51 +0100

    moodeng loses collision

--- Assets/moo_deng.gd
+++ Assets/moo_deng.gd
@@ -3,8 +3,6 @@ extends CharacterBody2D
 @export var speed = 1500
 var screen_size
 
-signal hit
-
 func _ready():
 	screen_size = get_viewport_rect().size
 	
@@ -12,13 +10,13 @@ func _ready():
 func _process(delta):
 	var hippo_speed = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
-		hippo_speed.x += 1000
+		hippo_speed.x += speed
 	if Input.is_action_pressed("move_left"):
-		hippo_speed.x -= 1000
+		hippo_speed.x -= speed
 	if Input.is_action_pressed("move_down"):
-		hippo_speed.y += 1000
+		hippo_speed.y += speed
 	if Input.is_action_pressed("move_up"):
-		hippo_speed.y -= 1000
+		hippo_speed.y -= speed
 
 	position += hippo_speed * delta
 	
@@ -26,7 +24,3 @@ func _process(delta):
 		$Hippo.flip_h = true
 	else:
 		$Hippo.flip_h = false
-
-func _on_body_entered(body):
-	hide()
-	hit.emit()
