266250175ea947871b471ad6a89b74cefa5f6546
LTnano <124283968+LTnano@users.noreply.github.com>
2025-07-18 13:22:46 +0100

    moodeng do the michael

--- Assets/moo_deng.gd
+++ Assets/moo_deng.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 1500
+var hippo_speed = Vector2.ZERO
 var screen_size
 
 func _ready():
@@ -8,18 +9,24 @@ func _ready():
 	
 
 func _process(delta):
-	var hippo_speed = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		hippo_speed.x += speed
+		hippo_flip()
 	if Input.is_action_pressed("move_left"):
 		hippo_speed.x -= speed
+		hippo_flip()
 	if Input.is_action_pressed("move_down"):
 		hippo_speed.y += speed
 	if Input.is_action_pressed("move_up"):
 		hippo_speed.y -= speed
 
 	position += hippo_speed * delta
+	hippo_speed = Vector2.ZERO
 	
+	# slide backwards like michael jackson
+	
+
+func hippo_flip():
 	if hippo_speed.x < 0:
 		$Hippo.flip_h = true
 	else:
