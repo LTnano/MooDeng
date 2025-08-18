fa264e34a495988e4eb9902eeebfac9506440f7c
LTnano <124283968+LTnano@users.noreply.github.com>
2025-08-18 14:33:46 +0100

    Added switch statement and resizer for the fruit

--- Assets/balls.gd
+++ Assets/balls.gd
@@ -4,7 +4,8 @@ signal yumyum
 
 const BallTypes = preload("res://Assets/ball_types.gd")
 
-@onready var eatingBalls = $CollisionShape2D
+@onready var bouncyBalls = $CollisionShape2D
+@onready var eatingBalls = $BallDetector/BallDetectorShape
 @onready var animated_sprite: AnimatedSprite2D = $BallSelectionBox
 
 var my_type: int
@@ -16,14 +17,8 @@ func _ready():
 	
 	# Get the string name
 	var type_name = BallTypes.get_ball_type_name(my_type)
-	
-	# Check if animation exists in the SpriteFrames resource and play
-	var frames_resource = animated_sprite.sprite_frames
-	if frames_resource and frames_resource.has_animation(type_name):
-		animated_sprite.animation = type_name
-		animated_sprite.play()
-	else:
-		print("No animation found for type:", type_name)
+
+	_initialise_ball_type(type_name)
 
 func yum():
 	emit_signal("yumyum")
@@ -32,3 +27,34 @@ func _on_ball_detector_body_entered(body: Node2D) -> void:
 	if body is mooDeng:
 		yum()
 		queue_free()
+
+func _initialise_ball_type(type_name: String):
+	
+	match type_name:
+		"Apple":
+			animated_sprite.play("Apple")
+			var ballBoxScaler = Vector2(0.5, 0.5)
+			eatingBalls.apply_scale(ballBoxScaler)
+			bouncyBalls.apply_scale(ballBoxScaler)
+		"Blueberry":
+			animated_sprite.play("Blueberry")
+			var ballBoxScaler = Vector2(0.3, 0.3)
+			eatingBalls.apply_scale(ballBoxScaler)
+			bouncyBalls.apply_scale(ballBoxScaler)
+		"Cherry":
+			animated_sprite.play("Cherry")
+			var ballBoxScaler = Vector2(0.3, 0.3)
+			eatingBalls.apply_scale(ballBoxScaler)
+			bouncyBalls.apply_scale(ballBoxScaler)
+		"Peach":
+			animated_sprite.play("Peach")
+			var ballBoxScaler = Vector2(0.3, 0.3)
+			eatingBalls.apply_scale(ballBoxScaler)
+			bouncyBalls.apply_scale(ballBoxScaler)
+		"Watermelon":
+			animated_sprite.play("Watermelon")
+			var ballBoxScaler = Vector2(1, 1)
+			eatingBalls.apply_scale(ballBoxScaler)
+			bouncyBalls.apply_scale(ballBoxScaler)
+		_:
+			print("Unknown ball type:", type_name)
